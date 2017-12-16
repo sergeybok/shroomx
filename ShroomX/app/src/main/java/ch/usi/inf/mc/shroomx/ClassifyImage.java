7664fb007b44ee5ef335988aad235404c4d02944
Sergey Bokhnyak <sergeybok@gmail.com>
2017-12-16 16:11:05 +0100

    Adding some changes and frozen graphs

---
+++ ShroomX/app/src/main/java/ch/usi/inf/mc/shroomx/ClassifyImage.java
@@ -0,0 +1,15 @@
+package ch.usi.inf.mc.shroomx;
+
+/**
+ * Created by sergiybokhnyak on 15.12.17.
+ */
+
+public class ClassifyImage {
+
+    public static String Classify() {
+
+        return "";
+
+    }
+
+}
--- ShroomX/app/src/main/java/ch/usi/inf/mc/shroomx/TermsConditionsFragment.java
+++ ShroomX/app/src/main/java/ch/usi/inf/mc/shroomx/TermsConditionsFragment.java
@@ -1,6 +1,11 @@
 package ch.usi.inf.mc.shroomx;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,12 +18,18 @@ import android.widget.TextView;
  *
  */
 
-public class TermsConditionsFragment extends Fragment {
+public class TermsConditionsFragment extends Fragment implements SensorEventListener {
     private static final String TAG = "TermsConditionsActivity";
 
     private TextView mShakeToAgree;
 
     //private Animation anim;
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+    private long lastUpdate = 0;
+    private float last_x, last_y, last_z;
+    private static final int SHAKE_THRESHOLD = 600;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,7 +43,30 @@ public class TermsConditionsFragment extends Fragment {
         anim.setStartOffset(2000);
         anim.setRepeatCount(Animation.INFINITE);
 
+        senSensorManager = (SensorManager) getActivity().getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+
+
         return v;
     }
 
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    public void onSensorChange(SensorEvent sensorEvent) {
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+        }
+    }
 }
