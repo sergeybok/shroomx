2b665a17eb88b7ee27882e454b49a754c1703b95
Martin Dodevski <martindodevski@localhost.localdomain>
2017-12-19 11:34:06 +0100

    main and home activity

--- ShroomX/app/src/main/java/ch/usi/inf/mc/shroomx/HomeActivity.java
+++ ShroomX/app/src/main/java/ch/usi/inf/mc/shroomx/HomeActivity.java
@@ -1,13 +1,34 @@
 package shroomx.shroomx;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import static shroomx.shroomx.R.layout.activity_home2;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
+        setContentView(activity_home2);
     }
+/*
+public void MushroomDetailActivity(View view){
+    Intent mushIntend = new Intent(HomeActivity.this,MushroomDetailActivity);
+    startActivity(mushIntend);
+    finish();
+}
+    public void MushroomDetailActivity(View view){
+        Intent mushIntend = new Intent(HomeActivity.this,MushroomDetailActivity);
+        startActivity(mushIntend);
+        finish();
+    }
+    public void CameraActivity(View v) {
+        Intent camIntent = new Intent(HomeActivity.this, CameraActivity.class);
+        startActivity(camIntent);
+        finish();
+    }
+*/
 }
